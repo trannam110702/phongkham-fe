69f9e1b730805911669b6fef9413df3bcfc66575
nam <tnam5353@gmail.com>
2024-04-11 17:49:07 +0700

    nam up

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { message } from "antd";
 const axiosClient = axios.create({
-  baseURL: "http://localhost:4001",
+  baseURL: "http://localhost:3000",
   timeout: 20000,
   headers: {
     "Content-Type": "application/json",
