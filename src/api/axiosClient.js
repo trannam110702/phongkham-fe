36610106f6ae6d628d5a2f32dcfd2a35f7068f46
nam <tnam5353@gmail.com>
2024-04-11 17:56:03 +0700

    nam up

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { message } from "antd";
 const axiosClient = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://toylity.com/api",
+  // baseURL: "http://localhost:3000",
   timeout: 20000,
   headers: {
     "Content-Type": "application/json",
